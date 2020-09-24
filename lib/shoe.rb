e5b1cb0ad4976448240038dbf386055095468a3f
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-24 20:17:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end# Make your shoe class here!
